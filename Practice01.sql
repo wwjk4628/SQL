895e412e69883e0936a942db8278ac23f061c07c
wwjk4628 <163090464+wwjk4628@users.noreply.github.com>
2024-04-24 17:55:18 +0900

    Create Practice01.sql

---
+++ Practice01.sql
@@ -0,0 +1,63 @@
+--  문제1
+SELECT first_name ||' '|| last_name ,
+salary ,
+phone_number ,
+hire_date 
+FROM employees
+ORDER BY hire_date ASC;
+
+--  문제1
+SELECT first_name ||' '|| last_name as "이름",
+salary as "월급",
+phone_number as "전화번호",
+hire_date as "입사일"
+FROM employees
+ORDER BY hire_date ASC;
+
+-- 문제2
+SELECT job_title, max_salary
+FROM jobs
+ORDER BY max_salary DESC;
+
+-- 문제3
+SELECT first_name ||' '|| last_name, manager_id, commission_pct, salary
+FROM employees
+WHERE manager_id IS NOT NULL AND commission_pct IS NULL AND salary > 3000;
+
+-- 문제4
+SELECT job_title, max_salary
+FROM jobs
+WHERE max_salary >= 1000
+ORDER BY max_salary DESC;
+
+-- 문제5
+SELECT first_name, salary, nvl(commission_pct, 0)
+FROM employees
+WHERE salary BETWEEN 10000 AND 13999
+ORDER BY salary DESC;
+
+-- 문제6
+SELECT first_name, salary, TO_CHAR(hire_date,'YYYY-MM'), department_id
+FROM employees
+WHERE department_id IN (10, 90, 100);
+
+-- 문제 7
+SELECT first_name, salary
+FROM employees
+WHERE LOWER(first_name) LIKE '%s%';
+
+-- 문제8
+SELECT department_name
+FROM departments
+ORDER BY LENGTH(department_name) DESC;
+
+-- 문제9
+SELECT UPPER(country_name)
+FROM countries
+WHERE country_id IN(SELECT country_id FROM locations)
+ORDER BY country_name ASC;
+
+-- 문제10
+SELECT first_name, salary, TRANSLATE(phone_number,'.','-') as "전화번호" , hire_date
+FROM employees
+WHERE hire_date < TO_DATE('20131231','YYYYMMDD');
