4ffcab879aa2928b4d33298f247f8bbb3d94b528
wwjk4628 <163090464+wwjk4628@users.noreply.github.com>
2024-04-25 14:17:19 +0900

    Update 01-DML-SELECT.sql
    오전커밋

--- 01-DML-SELECT.sql
+++ 01-DML-SELECT.sql
@@ -213,3 +213,121 @@ SELECT department_id,
     FROM employees
     ORDER BY department_id ASC, salary DESC;
 --  정렬 기준을 어떻게 세우느냐에 따라 성능, 출력 결과 영향을 미칠 수 있다.
+
+
+---------------------------
+-- 단일행 함수
+----------------------
+-- 단일 레코드를 기준으로 특정 컬럼의 값에 적용되는 함수
+
+-- 문자열 단일행 함수
+SELECT first_name, last_name,
+    CONCAT(first_name, CONCAT(' ', last_name)),
+    first_name || ' ' || last_name,
+    INITCAP(first_name || ' ' || last_name)
+FROM employees;
+
+SELECT first_name, last_name,
+LOWER(first_name),
+UPPER(first_name),
+LPAD(first_name, 20, '*'),
+RPAD(first_name, 20, '*')
+FROM employees;
+
+SELECT '    Oracle  ',
+    '*****Database*****',
+    LTRIM('    Oracle  '), -- 왼쪽의 빈 공간 삭제
+    RTRIM('    Oracle  '),   -- 오른쪽의 빈 공간 삭제
+    TRIM('*' FROM '*****Database*****'),    -- 앞뒤의 잡음 문자 제거
+    LTRIM('*****Database*****','*'), --  왼쪽의 '*' 문자 제거
+    SUBSTR('Oracle Database', 8, 4),
+    SUBSTR('Oracle Database', -8, 4),
+    LENGTH('Oracle Database')   -- 문자열의 길이
+FROM dual;
+
+-- 수치형 단일행 함수
+
+SELECT 3.14159,
+ABS(-3.14), --  절대값
+CEIL(3.14), --  정수로 올림
+FLOOR(3.14),    -- 정수로 버림
+ROUND(3.5), -- 정수로 반올림
+ROUND(3.14159,3),    -- 소수점 3쨰 자리까지 반올림 (네쨰 자리에서 반올림)
+TRUNC(3.5),     --  버림
+TRUNC(3.14159,3),  --  소수점 네쨰 자리에서 버림
+SIGN(-3.14159),  --  부호 (-1: 음수, 0:0, 1: 양수)
+MOD(7,3),    --  7을 3으로 나눈 나머지
+POWER(2,4)  --  2의 4제곱
+FROM dual;
+
+--------------
+-- DATA FORMAT
+--------------
+-- 현재 세션 정보 확인
+SELECT * FROM nls_session_parameters;
+-- 현재 날짜 포맷이 어떻게 되는가?
+-- 딕셔너리를 확인
+SELECT value FROM nls_session_parameters
+WHERE parameter = 'NLS_DATE_FORMAT';
+-- session -> 현재 접속된 사용자의 환경정보
+
+-- 현재 날자 : SYSDATE
+SELECT sysdate FROM dual;   --  가상테이블 dual로 부터 받아오므로 1개의 레코드
+
+SELECT sysdate FROM employees;  --  employees 테이블로부터 받아오므로 employees테이블 레코드의 갯수만큼
+
+--  날자 관련 단일행 함수
+SELECT sysdate,
+ADD_MONTHS(sysdate, 2),  -- 2개월이 지난 후의 날자
+LAST_DAY(sysdate),   --  현재달의 마지막 날
+ROUND(MONTHs_BETWEEN(sysdate, LAST_DAY(sysdate))),
+ROUND(MONTHs_BETWEEN('12/09/24', sysdate)), --  두 날자 사이의 개월 차
+NEXT_DAY(sysdate,6), -- 일:1 ~ 토:7   다음 금요일
+ROUND(sysdate, 'MONTH'),
+TRUNC(sysdate, 'MONTH')
+FROM dual;
+
+SELECT first_name, hire_date,
+ROUND(MONTHS_BETWEEN(sysdate, hire_date),1) as "근속월수"
+FROM employees;
+
+---------------
+--  변환함수
+---------------
+
+-- TO_NUMBEr(s, fmt) : 문자열 -> 숫자
+-- TO_DATE(s, fmt) : 문자열 -> 날짜
+-- TO_CHAR(o, fmt) : 숫자, 날짜 -> 문자열
+
+-- TO_CHAR
+SELECT first_name, hire_date,
+TO_CHAR(hire_date, 'yyyy-MM-DD')
+FROM employees;
+
+-- 현재 시간을 년-월-일 시:분:초:로 표기
+SELECT sysdate,
+TO_CHAR(sysdate, 'YYYY-MM-DD HH:MI:SS')
+FROM dual;
+
+SELECT 
+TO_CHAR(3000000, 'L999,999,999,999.99')
+FROM dual;
+
+-- 모든 직원의 이름과 연봉 정보를 표시
+SELECT first_name, salary, commission_pct,
+TO_CHAR((salary + salary * nvl(commission_pct, 0))*12, '$999,999,999.99')
+FROM employees;
+
+-- 문자를 숫자로: TO_NUMBER\
+SELECT '$57,600',
+TO_NUMBER('$57,600.00', '$999,999.00')/12 월급
+FROM dual;
+
+-- 문자열 -> 날짜
+SELECT '2024년04월25일 13시48분00초',
+TO_DATE('2024년04월25일 13시48분00초', 'YYYY"년"MM"월"DD"일" HH24"시"MI"분"SS"초"')
+FROM dual;
+
+SELECT '2024-04-25 13:48:00',
+TO_DATE('2024-04-25 13:48:00', 'YYYY-MM-DD HH24:MI:SS')
+FROM dual;
