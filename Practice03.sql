f47cb0227b8fa64c427c45ab57e1cdeb7767aa52
wwjk4628 <163090464+wwjk4628@users.noreply.github.com>
2024-04-29 12:12:09 +0900

    Update Practice03.sql
    문제별로 ANSI 추가

--- Practice03.sql
+++ Practice03.sql
@@ -11,6 +11,17 @@ FROM employees e, departments d
 WHERE e.department_id = d.department_id
 ORDER BY 4 ASC,1 DESC;
 
+-- ANSI
+-- JOIN의 의도를 명확하게 하고, JOIN 조건과 SELECTION 조건을 분리하는 효과
+SELECT e.employee_id,
+    e.first_name,
+    e.last_name,
+    d.department_name
+FROM employees e
+JOIN departments d
+ON e.department_id = d.department_id
+ORDER BY 4 ASC,1 DESC;
+
 --문제2.
 --employees 테이블의 job_id는 현재의 업무아이디를 가지고 있습니다.
 --직원들의 사번(employee_id), 이름(firt_name), 급여(salary), 부서명(department_name), 현
@@ -24,17 +35,45 @@ SELECT e.employee_id,
     j.job_title
 FROM employees e, departments d, jobs j
 WHERE e.department_id = d.department_id AND e.job_id = j.job_id
-ORDER BY 5 ASC;
-
+ORDER BY 1 ASC;
+---------------------------------------------------------------------
+-- ANSI
+---------------------------------------------------------------------
+SELECT e.employee_id,
+    e.first_name,
+    e.salary,
+    d.department_name,
+    j.job_title
+FROM employees e
+JOIN departments d
+ON e.department_id = d.department_id
+JOIN jobs j
+ON e.job_id = j.job_id
+ORDER BY 1 ASC;
 
 --문제2-1.
 --문제2에서 부서가 없는 Kimberely(사번 178)까지 표시해 보세요
 --(107건)
-SELECT e.employee_id
-FROM employees e, departments d
-WHERE e.department_id = d.department_id(+);
-
-
+SELECT e.employee_id,
+    e.first_name,
+    e.salary,
+    d.department_name,
+    j.job_title
+FROM employees e, departments d, jobs j
+WHERE e.department_id = d.department_id (+) AND e.job_id = j.job_id
+ORDER BY 1 ASC;
+-------------------------------------------
+SELECT e.employee_id,
+    e.first_name,
+    e.salary,
+    d.department_name,
+    j.job_title
+FROM employees e
+LEFT OUTER JOIN departments d
+ON e.department_id = d.department_id
+JOIN jobs j
+ON e.job_id = j.job_id
+ORDER BY 1 ASC;
 --문제3.
 --도시별로 위치한 부서들을 파악하려고 합니다.
 --도시아이디, 도시명, 부서명, 부서아이디를 도시아이디(오름차순)로 정렬하여 출력하세요
@@ -46,6 +85,15 @@ SELECT l.location_id as "도시아이디",
     d.department_id as "부서아이디"
 FROM locations l , departments d
 WHERE l.location_id = d.location_id
+ORDER BY 1 ASC;
+----------------------------------------------
+SELECT l.location_id as "도시아이디",
+    l.city as "도시명",
+    d.department_name as "부서명",
+    d.department_id as "부서아이디"
+FROM locations l
+JOIN departments d
+ON l.location_id = d.location_id
 ORDER BY 1 ASC;
 
 
@@ -59,6 +107,15 @@ SELECT l.location_id,
 FROM locations l , departments d
 WHERE l.location_id  = d.location_id (+)
 ORDER BY 1 ASC;
+---------------------------------------------
+SELECT l.location_id as "도시아이디",
+    l.city as "도시명",
+    d.department_name as "부서명",
+    d.department_id as "부서아이디"
+FROM locations l
+LEFT OUTER JOIN departments d
+ON l.location_id = d.location_id
+ORDER BY 1 ASC;
 
 --문제4.
 --지역(regions)에 속한 나라들을 지역이름(region_name), 나라이름(country_name)으로 출력하
@@ -68,6 +125,14 @@ SELECT r.region_name as "지역이름",
     c.country_name as "나라이름"
 FROM regions r, countries c
 WHERE r.region_id = c.region_id
+ORDER BY 1 ASC, 2 DESC;
+----------------------------------------
+SELECT r.region_name as "지역이름",
+    c.country_name as "나라이름"
+FROM regions r
+
+JOIN countries c
+ON r.region_id = c.region_id
 ORDER BY 1 ASC, 2 DESC;
 
 
@@ -84,6 +149,7 @@ SELECT
     man.hire_date   매니저입사일
 FROM employees emp, employees man
 WHERE emp.manager_id = man.employee_id
+
 --자신의 매니저보다 채용일(hire_date)이 빠른 사원의
 AND emp.hire_date < man.hire_date;
 
@@ -113,11 +179,22 @@ ORDER BY 1 ASC;
 --(2건)
 SELECT e.employee_id as "사번", 
     e.first_name ||' '|| e.last_name as "이름", 
-    e.job_id as "업무아이디", 
+    j.job_id as "업무아이디", 
     j.start_date as "시작일", 
     j.end_date as "종료일"
 FROM employees e, job_history j
 WHERE e.employee_id = j.employee_id
+AND LOWER(j.job_id) LIKE 'ac_account';
+
+--------------------------------------------------------
+SELECT e.employee_id as "사번", 
+    e.first_name ||' '|| e.last_name as "이름", 
+    j.job_id as "업무아이디", 
+    j.start_date as "시작일", 
+    j.end_date as "종료일"
+FROM employees e
+JOIN job_history j
+ON e.employee_id = j.employee_id
 AND LOWER(j.job_id) LIKE 'ac_account';
 
 
@@ -126,20 +203,37 @@ AND LOWER(j.job_id) LIKE 'ac_account';
 --매니저(manager)의 이름(first_name), 위치(locations)한 도시(city), 나라(countries)의 이름
 --(countries_name) 그리고 지역구분(regions)의 이름(resion_name)까지 전부 출력해 보세요.
 --(11건)
-SELECT d.department_id,
-    d.department_name,
-    d.manager_id,
-    e.first_name,
-    l.city,
-    c.country_name,
-    r.region_name
+SELECT d.department_id as "부서번호",
+    d.department_name as "부서이름",
+    d.manager_id as "매니저번호",
+    e.first_name as "매니저이름",
+    l.city as "위치한도시",
+    c.country_name as "나라",
+    r.region_name as "지역"
 FROM employees e, departments d, locations l, countries c, regions r
 WHERE e.employee_id = d.manager_id
 AND d.location_id = l.location_id
 AND l.country_id = c.country_id
 AND c.region_id = r.region_id;
 
-
+--------------------------------------------------------------
+SELECT d.department_id as "부서번호",
+    d.department_name as "부서이름",
+    d.manager_id as "매니저번호",
+    e.first_name as "매니저이름",
+    l.city as "위치한도시",
+    c.country_name as "나라",
+    r.region_name as "지역"
+FROM employees e
+JOIN departments d
+ON e.employee_id = d.manager_id
+JOIN locations l
+ON d.location_id = l.location_id
+JOIN countries c
+ON l.country_id = c.country_id
+JOIN regions r
+ON c.region_id = r.region_id
+ORDER BY 1;
 
 --문제9.
 --각 사원(employee)에 대해서 사번(employee_id), 이름(first_name), 부서명
@@ -152,7 +246,19 @@ SELECT e.employee_id,
     e.manager_id,
     man.first_name
 FROM employees e, departments d ,employees man
-WHERE  e.manager_id = man.employee_id AND man.department_id = d.department_id(+)
+WHERE  e.manager_id = man.employee_id AND e.department_id = d.department_id(+)
+ORDER BY e.employee_id;
+-------------------------------------------------------------------------
+SELECT e.employee_id as "사번",
+    e.first_name as "이름",
+    d.department_name as "부서명",
+    e.manager_id as "매니저아이디",
+    man.first_name as "매니저이름"
+FROM employees e
+JOIN employees man
+ON e.manager_id = man.employee_id
+LEFT OUTER JOIN departments d
+ON e.department_id = d.department_id
 ORDER BY e.employee_id;
 ----------------------------------------------------------------------
 --SELECT
